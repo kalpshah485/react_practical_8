61f3b39a1aefea009fc4f570c8f6fd6c54dcc294
kalpshah485 <kalpshah485@gmail.com>
2022-03-25 12:17:52 +0530

    feat:Added router setup.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import routes from './config/routes';
 
 function App() {
   const state = useSelector(state => state);
   console.log(state);
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+      <Routes>
+        {
+          Object.keys(routes).map(key => {
+            const value = routes[key];
+            return <Route {...value} key={key} />
+          })
+        }
+        <Route path='/' element={<Navigate to='/home' />} />
+      </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
