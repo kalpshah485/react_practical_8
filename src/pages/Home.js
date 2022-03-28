7e7ef525c011f374857e6c1f78776551e3f06380
kalpshah485 <kalpshah485@gmail.com>
2022-03-28 09:58:14 +0530

    feat:Added signup page UI with some functionality.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const {user} = useSelector(state => state);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate('/signup');
+    }
+  }, [user,navigate])
+  
   return (
     <div>Home</div>
   )
